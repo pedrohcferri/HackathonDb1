aaca09f7ea4ce303a739ba4409c8532bf987bbfd
Pedro Henrique Carvalho Ferri <pedro.ferri@db1.com.br>
2024-05-28 17:56:19 -0300

    ultimo commitdb1

--- src/componentes/Pesquisa/dadosPesquisa.js
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -9,32 +9,32 @@ export const produtos = [
     {
         src: tenis_adidas,
         nome: 'Tênis Adidas - Preto',
-        valor: 350,80 
-    }
+        valor: 350.80 ,
+    },
 
- {
+   {
         src: camiseta,
         nome: 'Camiseta básica - Amarelo',
-        valor: 120,90
+        valor: 120.90,
     },
     {
         src: jaqueta_Colorblock_Amarelo,
         nome: "Jaqueta amarela - Listras azuis" ,
-        valor: 219,90   
+        valor: 219.90 ,
     },
     {
         src: moletom_Capuz_Zebra,
         nome: "Moletom com capuz - Estilo zebra",
-        valor: 230,50
+        valor: 230.50,
     },
     {
         src: camisa_Argentina,
         nome: "Camisa da Argentina",
-        valor: 149,90   
+        valor: 149.90 ,  
     },
     {
         src: camisa_Manchester,
         nome:"Camisa Do Manchster",
-        valor:168,90   
+        valor:168.90,   
     }
 ]
