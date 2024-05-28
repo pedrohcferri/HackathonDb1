6b0d6343a345fd3df7cd321908880b6832a514f7
Pedro Henrique Carvalho Ferri <pedro.ferri@db1.com.br>
2024-05-28 17:45:46 -0300

    commit com produtos

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,27 +1,40 @@
 import Input from "../Input"
 import styled from "styled-components"
+import { useState} from 'react'
+// import { produtos} from'./dadosPesquisa'
 
 const PesquisaContainer = styled.section`
-    display:flex;
-    align-items: center;
-    color: black;
+    color: #0b0b0cf6;
     text-align: center;
-    padding: 2rem 2rem 2rem 34rem;
-    height:20px;
+    padding: 30px 10px;
+    height: 470px;
     width: 100%;
+    flex-direction:column;
 `
 
 const Titulo = styled.h2`
+    padding: 20px 10px
     color: black;
     font-size: 36px;
     text-align: center;
     width: 30%;
 `
+const Paragrafo = styled.p`
+    padding: 20px 10px
+    display: flex;
+    color: black;
+    width: 30%;
+
+`
 function Pesquisa (){
+    const  [textoDigitado, setTextoDigitado]= useState('')
     return(
         <PesquisaContainer>
-            {/* <Titulo>Encontre aqui!</Titulo> */}
-            <Input placeholder="ENCONTRE AQUI!"/>
+            <Titulo>Encontre aqui!</Titulo>
+            <Input placeholder="ENCONTRE AQUI!"
+            onBlur = { evento => setTextoDigitado(evento.target.value) }
+            />
+             <Paragrafo>{textoDigitado}</Paragrafo>
         </PesquisaContainer>
     )
 }
